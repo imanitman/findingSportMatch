1e59290b7afd432dc36ecb233b8ec37299abe736
Đỗ Văn Hải <haidv28062004@gmail.com>
2024-08-03 21:46:09 +0700

    hoàn thiện giao diện thanh ngày tháng

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import EV from "./Component/Evting/EV.jsx";
 import Header from "./Component/Header/Header";
 import MainPicture from "./Component/MainPicture/MainPicture";
 import Matches from "./Component/matches/matches.jsx";
+import Page2DateBar from "./Component/Page2Detail/Page2DateBar.jsx";
 
 function App() {
   return (
     <>
-      <Header></Header>
-      <MainPicture></MainPicture>
+      {/* <Header></Header> */}
+      <Page2DateBar />
+      {/* <MainPicture></MainPicture>
       <Matches></Matches>
-      <EV></EV>
+      <EV></EV> */}
+
     </>
   )
 }
--- src/Component/page2_detail/date_component.jsx
+++
@@ -1,3 +0,0 @@
-alksdnjlkasdlka
-asldkmnalsd
-aklsdnlksa
