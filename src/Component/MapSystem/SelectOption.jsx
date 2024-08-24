0e109e9648947566d2b306a647be7ed9f14741f1
Đỗ Văn Hải <haidv28062004@gmail.com>
2024-08-24 21:24:47 +0700

    chả hiểu commit về cái gì luôn

--- src/Component/MapSystem/SelectOption.jsx
+++ src/Component/MapSystem/SelectOption.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { ProvinceAPI, DistrictAPI, CommuneAPI } from "./APITinhThanh";
+import React from "react";
 
 function SelectOption(props) {
 
